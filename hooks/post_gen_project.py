a99e43bf0ce89805c1acd8c8e35d7cedc818b01d
Jesse Sun <jessera@outlook.com>
2022-08-11 11:01:59 -0400

    test

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,8 +1,6 @@
 import os
-import shutil
 
 if __name__ == "__main__":
     if "{{cookiecutter.create_subdir}}" == "y":
         os.mkdir('app/services/{{cookiecutter.module_name}}')
-        #subprocess.call(['mkdir', 'app/services/{{cookiecutter.module_name}}'])
-        shutil.move('app/services/{{cookiecutter.service_name}}.rb', 'app/services/{{cookiecutter.module_name}}/{{cookiecutter.service_name}}.rb')
+        os.rename('app/services/{{cookiecutter.service_name}}.rb', 'app/services/{{cookiecutter.module_name}}/{{cookiecutter.service_name}}.rb')
