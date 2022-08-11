5755073f36dcb8e40de94d70eb2f73cea8b1af09
Jesse Sun <jessera@outlook.com>
2022-08-11 10:52:41 -0400

    test

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,6 +1,8 @@
 import subprocess
+import os
 
 if __name__ == "__main__":
     if "{{cookiecutter.create_subdir}}" == "y":
-        subprocess.call(['mkdir', 'app/services/{{cookiecutter.module_name}}'])
+        os.mkdir('app/services/{{cookiecutter.module_name}}')
+        #subprocess.call(['mkdir', 'app/services/{{cookiecutter.module_name}}'])
         subprocess.call(['mv', 'app/services/{{cookiecutter.service_name}}.rb', 'app/services/{{cookiecutter.module_name}}'])
