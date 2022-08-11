a2a46b4ae52ca49535ee5c46d9e19ad44bde5ed5
Jesse Sun <jessera@outlook.com>
2022-08-11 11:26:38 -0400

    test

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-if [ {{cookiecutter.create_subdir}} == "y" ]; then
+#!/bin/bash
+if [ {{cookiecutter.create_subdir}} == "y" ] then
   mkdir app/services/{{cookiecutter.module_name}}
   mv app/services/{{cookiecutter.service_name}}.rb app/services/{{cookiecutter.module_name}}
 fi
