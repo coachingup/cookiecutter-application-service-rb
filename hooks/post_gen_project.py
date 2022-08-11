7e0d9984176f70e10d19bbc4bda3e74083e38fb7
Jesse Sun <jessera@outlook.com>
2022-08-11 11:19:57 -0400

    changed to sh script

--- hooks/post_gen_project.py
+++
@@ -1,6 +0,0 @@
-import os
-
-if __name__ == "__main__":
-    if "{{cookiecutter.create_subdir}}" == "y":
-        os.mkdir('app/services/{{cookiecutter.module_name}}')
-        os.rename('app/services/{{cookiecutter.service_name}}.rb', 'app/services/{{cookiecutter.module_name}}/{{cookiecutter.service_name}}.rb')
